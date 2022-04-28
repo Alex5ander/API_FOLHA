ea1a18e55294a34c6f9adb19e1d1d628c98a95d7
Alexsander <alexsandergutierreez.com>
2022-04-27 22:20:45 -0300

    first commit

---
+++ API_FOLHA/CalculoSalarioAposentado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_FOLHA
+{
+    public class CalculoSalarioAposentado : ICalculoSalario
+    {
+        public string Nome { get; set; }
+        public long Cod { get; set; }
+
+        public CalculoSalarioAposentado(long Cod, string Nome) {
+            this.Cod = Cod;
+            this.Nome = Nome;
+        }
+
+        public double Calcular(double SalarioHora)
+        {
+            return SalarioHora * 10;
+        }
+
+        public double Imposto(double valor)
+        {
+            return valor * 0.05;
+        }
+
+        public void Imprimir()
+        {
+            
+        }
+    }
+}
