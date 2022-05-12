7068d99e6118f57be7d03abccdae98dd56b0ff47
Alexsander <alexsandergutierreez@gmail.com>
2022-05-11 21:54:45 -0300

    Aula

--- API_FOLHA/CalculoSalarioEstagiario.cs
+++ API_FOLHA/CalculoSalarioEstagiario.cs
@@ -6,31 +6,35 @@ using System.Threading.Tasks;
 
 namespace API_FOLHA
 {
-    public class CalculoSalarioAposentado : ICalculoSalario
+    public class CalculoSalarioEstagiario : ICalculoSalario 
     {
+
         public string Nome { get; set; }
         public long Cod { get; set; }
+        public double Ajuda { get; set; }
 
-        public CalculoSalarioAposentado(long Cod, string Nome) {
+        public CalculoSalarioEstagiario(long Cod, string Nome, double Ajuda)
+        {
             this.Cod = Cod;
             this.Nome = Nome;
+            this.Ajuda = Ajuda;
         }
 
         public double Calcular(double SalarioHora)
         {
-            return SalarioHora * 160 > 6050 ? 6050 : SalarioHora * 160;
+            return SalarioHora * 80 > 1212 ? 1212 : SalarioHora * 80 + Ajuda;
         }
 
         public double Imposto(double valor)
         {
-            return valor * 0.01;
+            return 0;
         }
 
         public void Imprimir()
         {
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("HOLERITE - INATIVO");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("HOLERITE - ESTAGIÁRIO");
             Console.WriteLine("COD: {0} ", Cod);
             Console.WriteLine("Nome: {0}", Nome);
             double bruto = Calcular(160);
@@ -38,6 +42,7 @@ namespace API_FOLHA
             Console.WriteLine("SAL BRUTO: {0:C2}", bruto);
             Console.WriteLine("IMPOSTO: {0:C2}", desconto);
             Console.WriteLine("SAL LIQ: {0:C2}", bruto - desconto);
+            Console.WriteLine("Ajuda: {0:C2}", Ajuda);
         }
     }
 }
--- TESTE_FOLHA/Program.cs
+++ TESTE_FOLHA/Program.cs
@@ -10,13 +10,36 @@ namespace TESTE_FOLHA
     class Program
     {
         static List<ICalculoSalario> Empregados;
+        static List<ICalculoSalario> Aposentados;
+        static List<ICalculoSalario> Estagiarios;
 
         static void Main(string[] args)
         {
             Empregados = new List<ICalculoSalario>();
-            Empregados.Add(new CalculoSalarioFuncionario(1, "JOÂO NINGUEM DA SILVA", 32));
-            Empregados.Add(new CalculoSalarioFuncionario(1, "MARIA ALGUÉM DE OLIVEIRA", 32));
+            for (int i = 0; i < 50; i++)
+            {
+                Empregados.Add(new CalculoSalarioFuncionario(1, "JOÂO NINGUEM DA SILVA", 32));
+                Empregados.Add(new CalculoSalarioFuncionario(2, "MARIA ALGUÉM DE OLIVEIRA", 32));
+            }
+
             Empregados.ForEach(e => e.Imprimir());
+
+            Aposentados = new List<ICalculoSalario>();
+            for (int i = 0; i < 100; i++)
+            {
+                Aposentados.Add(new CalculoSalarioAposentado(i, "Nome do aposentado"));
+            }
+
+            Aposentados.ForEach(e => e.Imprimir());
+
+            Estagiarios = new List<ICalculoSalario>();
+            for (int i = 0; i < 100; i++)
+            {
+                Estagiarios.Add(new CalculoSalarioEstagiario(i, "Nome do aposentado", 100));
+            }
+
+            Estagiarios.ForEach(e => e.Imprimir());
+            
             Console.ReadKey();
         }
     }
